5ed183722f062ce8f32ededaac19e96b39aa9973
Manny <eadumbire@gmail.com>
2019-05-11 16:43:20 -0400

    Added comment

--- tmuxcopypanes.sh
+++ tmuxcopypanes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This will use the default session as target unless it is specified with the '-t' flag
 for window in $(tmux list-windows -a -F "#{window_id}")
 do 
 	tmux list-panes -t $window -F '#{window_name} #{pane_current_path} #{pane_current_command} #{window_layout}'
