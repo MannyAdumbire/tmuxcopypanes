a8c25a52ca6ff96ecd8857f9bac5fe91eefd4dba
Manny <eadumbire@gmail.com>
2019-05-22 13:08:39 -0400

    Changed output format to match tmuxinator yml format

--- tmuxcopypanes.sh
+++ tmuxcopypanes.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# This will use the default session as target unless it is specified with the '-t' flag
-for window in $(tmux list-windows -a -F "#{window_id}")
+#The the first command line argument to this script will be passed to the tmux '-t' flag as the target session 
+
+IFS=$'\n'
+
+[[ $1 ]] && TARGET_SESSION=$1 || TARGET_SESSION=0 
+
+
+
+for window in $(tmux list-windows  -F  "#{window_id} #{window_name} #{window_layout}" -t $TARGET_SESSION)
 do 
-	tmux list-panes -t $window -F '#{window_name} #{pane_current_path} #{pane_current_command} #{window_layout}'
+				windowid=$(cut -d " " -f 1 <<< $window)
+				windowname=$(cut -d " " -f 2 <<< $window)
+				echo -e "\t-"$windowname":"	
+				echo -e "\t\tlayout: "$(cut -d " " -f 3 <<< $window)
+				echo -e "\t\tpanes:"
+					
+				for pane in $(tmux list-panes -t $TARGET_SESSION:$windowid -F '#{pane_current_path} #{pane_current_command}')
+				do
+								
+								echo -e "\t\t\t-cd $(awk "{print $1}"<<< $pane)"
+								echo -e "\t\t\t-$(awk "{print $2}" <<< $pane)"
+				done
 done
